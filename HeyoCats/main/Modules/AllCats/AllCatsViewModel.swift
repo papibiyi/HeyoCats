96466580e18b557a61d60ccbacbccfeaa9de289b
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-07-11 22:06:28 +0100

    Add AllCatsViewModel

---
+++ HeyoCats/main/Modules/AllCats/AllCatsViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  AllCatsViewModel.swift
+//  HeyoCats
+//
+//  Created by Mojisola Adebiyi on 08/07/2021.
+//
+
+import Foundation
+import Combine
+
+class AllCatsViewModel: ObservableObject {
+    let webService: CatsWebService
+    
+    @Published var cats = [Cat]()
+    
+    init(webService: CatsWebService) {
+        self.webService = webService
+        getCats()
+    }
+    
+    func getCats() {
+        webService.getCatBreeds { response in
+            switch response {
+            case .success(let cats):
+                self.cats = cats
+            case .failure:
+                break
+            }
+        }
+    }
+    
+    func isFavotite(row: Int) -> Bool {
+        CatsPersistenceManager.shared.load().first(where: {$0.id == self.cats[row].id}) == nil ? false : true
+    }
+    
+    func saveCat(row: Int) -> Bool {
+        if isFavotite(row: row) {
+            CatsPersistenceManager.shared.remove(cat: cats[row])
+            return false
+        }else {
+            CatsPersistenceManager.shared.save(cat: cats[row])
+            return true
+        }
+    }
+
+}
