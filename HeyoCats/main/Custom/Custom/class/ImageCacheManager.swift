937af0d4d744c0ac2250db9c061580049d768ad4
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-07-11 22:07:44 +0100

    Update ImageCacheManager

--- HeyoCats/main/Custom/Custom/class/ImageCacheManager.swift
+++ HeyoCats/main/Custom/Custom/class/ImageCacheManager.swift
@@ -10,20 +10,30 @@ import Foundation
 /// Responsible for caching fetching and caching images by their URL strings
 class ImageCacheManager {
     static let cache = NSCache<NSString, NSData>()
+    static let session = URLSession(configuration: URLSessionConfiguration.default)
+    static var taskDictionary: [String: URLSessionDataTask] = [:]
 
-    static func fetchImageData(from url: String, completion: @escaping (NSData) -> (Void)){
-        let key = NSString(string: url)
-        if let cachedVersion = cache.object(forKey: key) {
-            DispatchQueue.global(qos: .background).async {
-                completion(cachedVersion)
-            }
-        } else {
-            DispatchQueue.global(qos: .background).async {
+    static func fetchImageData(with uuid: String, from url: String, completion: @escaping (NSData) -> (Void)){
+        DispatchQueue.global(qos: .background).async {
+            let task = ImageCacheManager.taskDictionary[uuid]
+            task?.cancel()
+            let key = NSString(string: url)
+            if let cachedVersion = ImageCacheManager.cache.object(forKey: key) {
+                    completion(cachedVersion)
+            } else {
                 guard let url = URL(string: url) else {return}
-                guard let ddata = try? Data(contentsOf: url) else {return}
-                let ns = NSData(data: ddata)
-                cache.setObject(ns, forKey: key)
-                completion(ns)
+                let task =  ImageCacheManager.session.dataTask(with: url) { data, _, _ in
+                    guard let data = data else {return}
+                    let ns = NSData(data: data)
+                    ImageCacheManager.cache.setObject(ns, forKey: key)
+                    completion(ns)
+                }
+                
+                DispatchQueue.main.sync {
+                    ImageCacheManager.taskDictionary.updateValue(task, forKey: uuid)
+                }
+                    
+                task.resume()
             }
         }
     }
